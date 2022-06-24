21fbbfb1dd3a72670b7f916195e0407c002fbc98
Yaroslav <yaros6666@yandex.ru>
2022-06-24 12:46:12 +0300

    add task 3

---
+++ Lecture_2/Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿int Find = 35;
+int count = 0;
+int[] array = { 6, 2, 6, 3, 35 };
+
+int n= array.Length;
+
+for (int i = 0; i < n; i++)
+{
+    if (array[i] == Find) count++;
+}
+
+if (count > 0)
+{
+    Console.WriteLine($"Sucsesful. {count} Times");
+}
+
+else
+{
+    Console.WriteLine("Not Sucsesful");
+}
+
