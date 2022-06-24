4a4f236edd230c309ecaf6403abbf458efb22d21
Yaroslav <yaros6666@yandex.ru>
2022-06-24 17:39:30 +0300

    add task 4

---
+++ Lecture_2/Task_4/Program.cs
@@ -0,0 +1,28 @@
+﻿void fillArray(int [] myArray)
+{
+    int length = myArray.Length;
+    int index = 0;
+    while (index < length)
+    {
+        myArray[index] = new Random().Next(0, 10);
+        index ++;
+    }
+}
+
+void PrintArray(int [] col)
+{
+    int pos = 0;
+    int len = col.Length;
+    while (pos < len)
+    {
+        Console.WriteLine(col[pos]);
+        pos ++;
+    } 
+}
+
+int [] array = new int [10];
+
+fillArray(array);
+PrintArray(array);
+
+
