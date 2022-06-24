5e4d781eef9e03ab5685d4cbc790c0ba31617bba
Yaroslav <yaros6666@yandex.ru>
2022-06-24 18:11:52 +0300

    add task 4.1

--- Lecture_2/Task_4/Program.cs
+++ Lecture_2/Task_4/Program.cs
@@ -20,9 +20,33 @@ void PrintArray(int [] col)
     } 
 }
 
+int indexOf(int [] array1, int Find)
+{
+    int count = array1.Length;
+    int index = 0;
+    int posit = -1;
+    while(index<count)
+    {
+        if (array1[index]==Find)
+        {
+            posit = index;
+            break;
+            
+        }
+    index++; 
+    }
+    return posit;
+}
+
 int [] array = new int [10];
 
 fillArray(array);
+array[4]=45;
+array[5]=45;
 PrintArray(array);
 
+int myIndex = indexOf(array,45);
+
+Console.WriteLine();
+Console.WriteLine(myIndex);
 
