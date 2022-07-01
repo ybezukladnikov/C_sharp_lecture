9e291cdc0f8e5ec9662c65f619f00831b87d269a
Yaroslav <yaros6666@yandex.ru>
2022-07-01 12:06:32 +0300

    add

---
+++ Lecture_3/Program.cs
@@ -0,0 +1,32 @@
+﻿string test = "ooonkhr";
+
+void findChar(string arg)
+{
+    string result =String.Empty;
+    string o ="o";
+    string O ="O";
+
+    for (int i =0; i< arg.Length; i++)
+    {
+        if (arg[i]==o[0])
+        {
+            result = result+ O;
+        }
+
+        else
+        {
+            result = result+ arg[i];
+        }
+    }
+
+
+    Console.WriteLine(result);
+    
+}
+
+
+findChar(test);
+
+int year = DateTime.Now.Year;
+Console.WriteLine(year);
+
